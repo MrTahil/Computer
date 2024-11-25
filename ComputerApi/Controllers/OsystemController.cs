692091622f0c5c879cd85a36b913cb96143b4cf3
MrTahil <hiloczkit@kkszki.hu>
2024-11-25 08:39:19 +0100

    Os post is ready

---
+++ ComputerApi/Controllers/OsystemController.cs
@@ -0,0 +1,33 @@
+﻿using ComputerApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerApi.Controllers
+{   
+    [Route("osystem")]
+        [ApiController]
+    public class OsystemController : Controller
+    {
+        private readonly ComputerContext _context;
+
+        public OsystemController(ComputerContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public ActionResult<Osystem> Post(CreateOsDto osdto)
+        {
+            var os = new Osystem
+            {
+                Id =Convert.ToString( Guid.NewGuid()),
+                Name = osdto.name
+            };
+            if (os != null) { 
+                _context.Osystems.Add(os);
+                _context.SaveChanges();
+                return StatusCode(201, os);
+            }
+            
+            return BadRequest();
+        }
+    }
+}
